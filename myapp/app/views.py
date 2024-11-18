751ada02d3e2f7fa3b407c15b484bbdd3a417a80
Ayana <getuh775@gmail.com>
2024-11-18 07:44:36 -0800

    commited my free time activity

--- myapp/app/views.py
+++ myapp/app/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 from django.http import JsonResponse
 def name(request):
     return HttpResponse("l'm Getu Haile")
+def hobby(request):
+    response_text = 'name: Getu Haile, football: playing and watching football, read: reading books'
+    return HttpResponse(response_text)
