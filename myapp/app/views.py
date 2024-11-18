bf7996e5338fd692261ba5bbaa2ef7a7c2122202
Ayana <getuh775@gmail.com>
2024-11-18 07:52:31 -0800

    my dream of life

--- myapp/app/views.py
+++ myapp/app/views.py
@@ -7,3 +7,5 @@ def name(request):
 def hobby(request):
     response_text = 'name: Getu Haile, football: playing and watching football, read: reading books'
     return HttpResponse(response_text)
+def dream(request):
+    return HttpResponse("My dream is to become a skilled computer scientist, leveraging technology to create innovative solutions and improve lives through software and systems development")
